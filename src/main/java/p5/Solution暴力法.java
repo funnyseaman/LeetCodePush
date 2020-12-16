5fe1ee3f9aeda4145a2bbf3c1f73c64e86fbfcdb
sinanfan <sinanfan@tencent.com>
2020-12-16 19:50:59 +0800

    Solution_5

---
+++ src/main/java/p5/Solution暴力法.java
@@ -0,0 +1,41 @@
+package p5;
+
+public class Solution暴力法 {
+    public String longestPalindrome(String s) {
+        int length = s.length();
+        if (length < 2) {
+            return s;
+        }
+        int maxLength = 1;
+        int begin = 0;
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (isPalindrome(chars, i, j) && (j - i + 1) > maxLength) {
+                    maxLength = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLength);
+    }
+
+    boolean isPalindrome(char[] chars, int left, int right) {
+        while (left < right) {
+            if (chars[left] != chars[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution暴力法 so = new Solution暴力法();
+        String s = "babad";
+        String res = so.longestPalindrome(s);
+        System.out.println(res);
+    }
+}
+
---
+++ src/test/java/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        String s = "abcd";
+        System.out.println(s.substring(0, 1));
+    }
+}
