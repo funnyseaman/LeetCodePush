61a1de5c0925f11ea2c2c48337936737fac89f95
funnyseaman <funnyseaman@163.com>
2020-12-31 15:48:56 +0800

    null

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,6 +1,5 @@
+import java.io.IOException;
+
 public class Test {
-    public static void main(String[] args) {
-        String s = "abcd";
-        System.out.println(s.substring(0, 1));
-    }
+
 }
