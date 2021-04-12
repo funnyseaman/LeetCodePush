cf7407f668d1dcb229626b687a1f339279e92113
funnyseaman <funnyseaman@163.com>
2021-04-12 18:22:32 +0800

    (u) InsertionSort

--- src/main/java/learning/algorithms/sort/InsertionSort.java
+++ src/main/java/learning/algorithms/sort/InsertionSort.java
@@ -1,7 +1,30 @@
 package learning.algorithms.sort;
 
+import util.PrettyFormat;
+
 public class InsertionSort {
     public static void sort(int[] a) {
+        int[] res;
+        for (int i = 0; i < a.length; i++) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{6, 2, 7, 3, 5, 8, 1, 3};
+        PrettyFormat.print(a);
+        InsertionSort.sort(a);
+        PrettyFormat.print(a);
+
+        a = new int[]{1, 5, 3, 7, 9};
+        PrettyFormat.print(a);
+        InsertionSort.sort(a);
+        PrettyFormat.print(a);
+
+        a = new int[]{};
+        PrettyFormat.print(a);
+        InsertionSort.sort(a);
+        PrettyFormat.print(a);
 
     }
 }
