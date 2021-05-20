6bc173aa26cc9f026f2942c8722375172470006c
funnyseaman <funnyseaman@163.com>
2021-05-20 18:15:40 +0800

    (+)

--- src/main/java/learning/algorithms/sort/MergeSort.java
+++ src/main/java/learning/algorithms/sort/MergeSort.java
@@ -4,7 +4,11 @@ import org.junit.Test;
 import util.PrettyFormat;
 
 public class MergeSort {
-    public static void sort(int[] arr) {
+    public static void sort(int[] arr, int[] result) {
+        assistSort(arr, result, 0, arr.length - 1);
+    }
+
+    private static void assistSort(int[] arr, int[] result, int start, int end) {
 
     }
 
@@ -13,17 +17,20 @@ public class MergeSort {
     public void testSort() {
         int[] a = new int[]{6, 2, 7, 3, 5, 8, 1, 3};
         PrettyFormat.print(a);
-        MergeSort.sort(a);
-        PrettyFormat.print(a);
+        int[] result = new int[a.length];
+        MergeSort.sort(a, result);
+        PrettyFormat.print(result);
 
         a = new int[]{1, 5, 3, 7, 9};
         PrettyFormat.print(a);
-        MergeSort.sort(a);
-        PrettyFormat.print(a);
+        result = new int[a.length];
+        MergeSort.sort(a, result);
+        PrettyFormat.print(result);
 
         a = new int[]{};
         PrettyFormat.print(a);
-        MergeSort.sort(a);
-        PrettyFormat.print(a);
+        result = new int[a.length];
+        MergeSort.sort(a, result);
+        PrettyFormat.print(result);
     }
 }
