3ff3ac6e8895d27d371df3104819c8674093b86f
funnyseaman <funnyseaman@163.com>
2021-07-08 01:23:37 +0800

    PrettyFormat增加了对泛型的支持，主要为了String的优雅格式输出

--- src/main/java/util/PrettyFormat.java
+++ src/main/java/util/PrettyFormat.java
@@ -9,6 +9,20 @@ public class PrettyFormat {
      *
      * @param a array
      */
+    public static <T> void print(T[] a) {
+        StringBuilder sb = new StringBuilder("[");
+        if (a.length > 0) {
+            for (T i : a) {
+                sb.append(i);
+                sb.append(", ");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        sb.append("]");
+        System.out.println(sb.toString());
+    }
+
     public static void print(int[] a) {
         StringBuilder sb = new StringBuilder("[");
         if (a.length > 0) {
