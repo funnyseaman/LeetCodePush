cb5d0062cac038a47ca02f23a02f35c0bb7da0fd
funnyseaman <funnyseaman@163.com>
2021-07-15 18:05:37 +0800

    206

---
+++ src/main/java/all/p206/Solution.java
@@ -0,0 +1,10 @@
+package all.p206;
+
+import bean.ListNode;
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode root = new ListNode(0, head);
+        return null;
+    }
+}
